ab6d2a12b53c4cbd7d195982f5efc5bc31d19f47
XiYang-Luo <279813lxyz>
2022-04-19 09:57:30 +0800

    refactor calculator to use reflect

--- src/main/java/ComputeFactory.java
+++ src/main/java/ComputeFactory.java
@@ -2,15 +2,16 @@ public class ComputeFactory {
     private static final Add add = new Add();
     private static final Sub sub = new Sub();
 
-    public static Computable getCompute(char symbol) {
-        switch (symbol){
-            case '+':
-                return add;
-            case '-':
-                return sub;
-            default:
-                throw new IllegalArgumentException();
-        }
+    public static Computable getCompute(String symbol) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+//        switch (symbol){
+//            case '+':
+//                return add;
+//            case '-':
+//                return sub;
+//            default:
+//                throw new IllegalArgumentException();
+//        }
+        return (Computable) Class.forName(symbol).newInstance();
     }
 
 
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,17 +5,17 @@ import static org.junit.Assert.*;
 
 public class CalculatorTest {
     @Test
-    public void shouldReturn2() {
+    public void shouldReturn2() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Calculator calculator = new Calculator();
-        int calculateRes = calculator.compute(1,1, '+');
+        int calculateRes = calculator.compute(1,1, "Add");
         int exceptionRes = 2;
         Assert.assertEquals(exceptionRes, calculateRes);
     }
 
     @Test
-    public void shouldReturn0() {
+    public void shouldReturn0() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Calculator calculator = new Calculator();
-        int calculateRes = calculator.compute(1,1, '-');
+        int calculateRes = calculator.compute(1,1, "Sub");
         int exceptionRes = 0;
         Assert.assertEquals(exceptionRes, calculateRes);
     }
